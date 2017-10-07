538e0e26b254fad263ff96cf65f0233622ddfd50
John Uberbacher <johnuberbacher@gmail.com>
2017-10-06 20:26:14 -0400

    More progress

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
-$('document').ready(function() {
-  var frontpage = "https://www.reddit.com/.json";
-  
-  $.getJSON(frontpage, function(data) {
-	$.each(
-	  data.data.children.slice(0, 15),
-	  
-	  function (i, post) {
-	    $("#data").append( '<div class="card"><div class="thumbnail"><a class="title" href="' + post.data.url + '"><img src="' + post.data.thumbnail + '"></a></div><div class="post"><p><a class="title" href="' + post.data.url + '">' + post.data.title + '</a></p><p>submitted by <a target="blank" class="author" href="https://reddit.com/user/' + post.data.author + '">' + post.data.author + '</a> to <a href="https://reddit.com/' + post.data.subreddit_name_prefixed +'">' + post.data.subreddit_name_prefixed + '</a></p><p><a target="blank" href="https://reddit.com' + post.data.permalink + '">View Comments</a></p></div></div>' );
-	  }
-	  
-	)
-  })
-  
-}); 
+function load(params) {
+  $.getJSON("http://www.reddit.com/.json", params, function (data) {
+    var children = data.data.children;
+      $.each(children, function (i, post) {
+        $("#data").append( '<div class="card"><div class="thumbnail"><a class="title" href="' + post.data.url + '"><img src="' + post.data.thumbnail + '"></a></div><div class="post"><p><a class="title" href="' + post.data.url + '">' + post.data.title + '</a></p><p>submitted by <a target="blank" class="author" href="https://reddit.com/user/' + post.data.author + '">' + post.data.author + '</a> to <a href="https://reddit.com/' + post.data.subreddit_name_prefixed +'">' + post.data.subreddit_name_prefixed + '</a></p><p><a target="blank" href="https://reddit.com' + post.data.permalink + '">View Comments</a></p></div></div>' );
+    });
+    if (children && children.length > 0) {
+        lastId = children[children.length - 1].data.id;
+    } else {
+        lastId = undefined;
+    }
+  });
+}
+
+function paginate() {
+  if (lastId) {
+    load({
+      after: 't3_' + lastId
+    });
+  }
+};
+
+window.onscroll = function(ev) {
+  if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+    setTimeout(function(){ 
+		paginate() 
+	}, 1000);
+  }
+};
+
+load();
